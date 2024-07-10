9ddd338bd331db1470aca1d24c7d505bf83d0ecd
ferarilalon <127186639+ferarilalon@users.noreply.github.com>
2024-07-10 23:04:11 +0600

    Add Calculator class with basic arithmetic operations

--- src/me/mjhasan/kotlin/oop/oop.kt
+++ src/me/mjhasan/kotlin/oop/oop.kt
@@ -1,5 +1,31 @@
 package me.mjhasan.kotlin.oop
 
 fun main() {
-    println("Object-Oriented Programming in Kotlin.")
+    val a: Int = 20
+    val b: Int = 0
+
+    val calculator = Calculator(a, b)
+    val add = calculator.addition()
+    val sub = calculator.subtraction()
+    val multiply = calculator.multiplication()
+    val divide = calculator.division()
+
+    println("$add, $sub, $multiply, ${divide ?: "Divide by zero not allowed!"}")
+}
+
+class Calculator(val a: Int, val b: Int) {
+    fun addition(): Int = a + b
+
+    fun subtraction(): Int = a - b
+
+    fun multiplication(): Int = a * b
+
+    fun division(): Int? {
+        return try {
+            a / b
+        } catch (e: ArithmeticException) {
+            println("${e.message} not allowed!")
+            null
+        }
+    }
 }
