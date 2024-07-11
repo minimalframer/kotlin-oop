81665e3853c74d591ce9765e115f5d6a8b65f032
ferarilalon <127186639+ferarilalon@users.noreply.github.com>
2024-07-11 22:51:31 +0600

    Add BaseSalariedEmployee class with salary details and enhanced Employee class
    - Created BaseSalariedEmployee class that inherits from Employee.
    - Added salary property to BaseSalariedEmployee and overridden getFullInfo method.
    - Enhanced Employee class with open getFullInfo method.
    - Updated main function to demonstrate usage of both Employee and BaseSalariedEmployee classes.

--- src/me/mjhasan/kotlin/oop/inheritance/inheritance.kt
+++ src/me/mjhasan/kotlin/oop/inheritance/inheritance.kt
@@ -1,21 +1,17 @@
 package me.mjhasan.kotlin.oop.inheritance
 
 fun main() {
-    println("Hello World")
-
     val employee = Employee("Muhammed", "Hasan")
     println(employee.getFullInfo())
 
     val bse = BaseSalariedEmployee(30000, "Muhammed", "Hasan")
     println(bse.getFullInfo())
-
-
 }
 
 open class Employee(val firstName: String, val lastName: String) {
     open fun getFullInfo() = "$firstName $lastName"
 }
 
-class BaseSalariedEmployee(var salary: Int, firstName: String, lastName: String): Employee(firstName, lastName) {
+class BaseSalariedEmployee(var salary: Int, firstName: String, lastName: String) : Employee(firstName, lastName) {
     override fun getFullInfo() = "$firstName $lastName draws $salary taka per month."
 }
